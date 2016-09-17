cb4fb934e392ea1300ac0fb8eaa3ede86b721610
Vinogradov <vinogradov.max49@gmail.com>
2016-09-17 23:22:37 +0300

    Few configuration rules were added

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
+# pass to .json file
 citiesJson = "cities/cities_en.json"
 
+# Redis db config
 redisHost = "localhost"
 redisPort = 6379
+
+# app_id for vk_api
+appID = 5603526
+# file, where auth data is saved
+authFile = '.auth_data'
+
+login = "*****"
+password = "*****"
+
+newMassagesLimit = 200
+
