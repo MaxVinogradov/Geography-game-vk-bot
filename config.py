8f52ac7b1ac1243740632f6c94aefceea2ba8a4a
Vinogradov <vinogradov.max49@gmail.com>
2016-09-18 19:53:32 +0300

    [#6] logic-module was finished.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ redisHost = "localhost"
 redisPort = 6379
 
 # app_id for vk_api
-appID = 00000
+appID = 0000
 # file, where auth data is saved
 authFile = '.auth_data'
 
@@ -15,7 +15,8 @@ botPhrases = {
     "startPhrase": "Hi! Game has been started by you.",
     "pausePhrase": "Hi! Game has been paused by you.",
     "resumePhrase": "Hi! Game has been resumed by you.",
-    "endPhrase": "Game has been ended by you. Bye!"
+    "endPhrase": "Game has been ended by you. Bye!",
+    "levelPhrase": "Your current level is "
 }
 
 # control phrases
@@ -23,7 +24,8 @@ controlPhrases = {
     "phraseToInitGame": "start",
     "phraseToPauseGame": "pause",
     "phraseToResumeGame": "resume",
-    "phraseToFinishGame": "finish"
+    "phraseToFinishGame": "finish",
+    "phraseToGetUsedCities": "cities"
 }
 
 # error phrases
@@ -31,5 +33,9 @@ errorPhrases = {
     "illegalStartingGame": "Error: game already started!",
     "illegalPauseGame": "Error: you can not pause the game without starting it or game has already paused!",
     "illegalResumingGame": "Error: you can not resume the game because you hadn't paused game before!",
-    "illegalFinishingGame": "Error: you can not finish the game without starting it!"
+    "illegalFinishingGame": "Error: you can not finish the game without starting it!",
+    "illegalLevel": "Error: level is not correct! It must be digit between 1 and 1000. Try again!",
+    "illegalWordOutOfGame": "Error: game hasn't started yet.",
+    "illegalCity": "Error: it is not city.",
+    "illegalRepeatedCity": "Error: we have already used this word in our game."
 }
--- logic.py
+++ logic.py
@@ -10,6 +10,7 @@ levels = {}
 
 
 def process():
+    sleep(3)
     while True:
         for message in vk.get_unreadable_message()[1:]:
             body = message["body"]
@@ -19,13 +20,19 @@ def process():
             check_pause(user_id, body)
             check_resume(user_id, body)
             check_finish(user_id, body)
-    sleep(3)
+            check_to_get_cities(user_id, body)
+            if body not in list(controlPhrases.values()):
+                if check_is_city(user_id, body):
+                    generate_new_word(user_id, body)
+
+        sleep(2)
 
 
 def check_start(user_id, body):
     if body == controlPhrases["phraseToInitGame"]:
         if user_id not in playersPool.keys():
             playersPool[user_id] = []
+            levels[user_id] = 100
             vk.send_message(user_id, botPhrases["startPhrase"])
         else:
             vk.send_message(user_id, errorPhrases["illegalStartingGame"])
@@ -62,7 +69,48 @@ def check_finish(user_id, body):
 
 def check_level(user_id, body):
     if body.startswith("level"):
-        levels[user_id] = body.split(" ")[1]
-        vk.send_message(user_id, botPhrases["levelPhrase"])
+        if len(body.split(" ")) == 2:
+            level = body.split(" ")[1]
+        else:
+            return
+        if level.isdigit() and 1 <= int(level) <= 1000:
+            levels[user_id] = level
+            vk.send_message(user_id, botPhrases["levelPhrase"] + level)
+        else:
+            vk.send_message(user_id, botPhrases["levelPhrase"] + str(levels[user_id]))
+            vk.send_message(user_id, errorPhrases["illegalLevel"])
+
+
+def check_to_get_cities(user_id, body):
+    if body == controlPhrases["phraseToGetUsedCities"] and user_id in playersPool.keys():
+        vk.send_message(user_id, playersPool[user_id])
+
+
+def check_is_city(user_id, body):
+    if body in playersPool[user_id]:
+        vk.send_message(user_id, errorPhrases["illegalRepeatedCity"])
+        return False
+    tmp = body.lower()
+    if user_id in playersPool.keys():
+        for i in range(1, 1001):
+            for j in db.smembers("en:{0}:{1}:{2}".format(i, tmp[0:1], tmp[-1:])):
+                if j.decode('utf-8') == body:
+                    playersPool[user_id].append(body)
+                    return True
+    else:
+        vk.send_message(user_id, errorPhrases["illegalWordOutOfGame"])
+    vk.send_message(user_id, errorPhrases["illegalCity"])
+    return False
+
+
+def generate_new_word(user_id, body):
+    tmp = playersPool[user_id][-1].lower()
+    for i in range(1, levels[user_id] + 1):
+        for lastLetter in range(ord("a"), ord("z")):
+            for j in db.smembers("en:{0}:{1}:{2}".format(i, tmp[-1:], chr(lastLetter))):
+                if j.decode('utf-8') not in playersPool[user_id]:
+                    playersPool[user_id].append(j.decode('utf-8'))
+                    vk.send_message(user_id,j.decode('utf-8'))
+                    return
 
 process()
