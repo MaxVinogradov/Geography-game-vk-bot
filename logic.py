d0ca8933411dbbe115dc83ebdb9bd1dac36f51f0
Vinogradov <vinogradov.max49@gmail.com>
2016-09-18 16:18:28 +0300

    [#6]Feature of level checking was added.

--- logic.py
+++ logic.py
@@ -1,10 +1,12 @@
 from integration import Integration
 from config import *
 from time import sleep
+from converter import db
 
 vk = Integration()
 playersPool = {}
 interruptedGamesPlayersIds = []
+levels = {}
 
 
 def process():
@@ -12,6 +14,7 @@ def process():
         for message in vk.get_unreadable_message()[1:]:
             body = message["body"]
             user_id = message["uid"]
+            check_level(user_id, body)
             check_start(user_id, body)
             check_pause(user_id, body)
             check_resume(user_id, body)
@@ -21,7 +24,7 @@ def process():
 
 def check_start(user_id, body):
     if body == controlPhrases["phraseToInitGame"]:
-        if user_id not in playersPool:
+        if user_id not in playersPool.keys():
             playersPool[user_id] = []
             vk.send_message(user_id, botPhrases["startPhrase"])
         else:
@@ -30,7 +33,7 @@ def check_start(user_id, body):
 
 def check_pause(user_id, body):
     if body == controlPhrases["phraseToPauseGame"]:
-        if user_id in playersPool and user_id not in interruptedGamesPlayersIds:
+        if user_id in playersPool.keys() and user_id not in interruptedGamesPlayersIds:
             interruptedGamesPlayersIds.append(user_id)
             vk.send_message(user_id, botPhrases["pausePhrase"])
         else:
@@ -39,7 +42,7 @@ def check_pause(user_id, body):
 
 def check_resume(user_id, body):
     if body == controlPhrases["phraseToResumeGame"]:
-        if user_id in playersPool and user_id in interruptedGamesPlayersIds:
+        if user_id in playersPool.keys() and user_id in interruptedGamesPlayersIds:
             interruptedGamesPlayersIds.remove(user_id)
             vk.send_message(user_id, botPhrases["resumePhrase"])
         else:
@@ -50,9 +53,16 @@ def check_finish(user_id, body):
     if body == controlPhrases["phraseToFinishGame"]:
         if user_id in interruptedGamesPlayersIds:
             interruptedGamesPlayersIds.remove(user_id)
-        if user_id in playersPool:
+        if user_id in playersPool.keys():
             playersPool.pop(user_id)
             vk.send_message(user_id, botPhrases["endPhrase"])
         else:
             vk.send_message(user_id, errorPhrases["illegalFinishingGame"])
 
+
+def check_level(user_id, body):
+    if body.startswith("level"):
+        levels[user_id] = body.split(" ")[1]
+        vk.send_message(user_id, botPhrases["levelPhrase"])
+
+process()
